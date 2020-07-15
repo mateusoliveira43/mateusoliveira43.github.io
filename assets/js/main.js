67a4a36b11e96d33bb4a2f71a3be5b3241efde70
mateusoliveira43 <matews1943@gamil.com>
2020-07-15 20:34:04 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,194 @@
+// Fazer um collapse aki depois
+const corpoContato = '<h1>Contato</h1>\
+<div ><a href="https://www.linkedin.com/in/mateus-oliveira-2492851b1/" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`linkedin`,true)" onmouseout="hoverDesativo(`linkedin`,true)"><img src="./assets/img/linkedin-desativado.svg" alt="logo linkedin" width="30px" id="linkedin-contato"/><div class="espaco-lateral"></div>Linkedin do Mateus</a></div><p class="par-contato">Descrição do Linkedin do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
+<div ><a href="https://github.com/mateusoliveira43" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`github`,true)" onmouseout="hoverDesativo(`github`,true)"><img src="./assets/img/github-desativado.svg" alt="logo github" width="30px" id="github-contato"/><div class="espaco-lateral"></div>GitHub do Mateus</a></div><p class="par-contato">Descrição do GitHub do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
+<div ><a href="mailto:matews1943@gmail.com" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`email`,true)" onmouseout="hoverDesativo(`email`,true)"><img src="./assets/img/email-desativado.svg" alt="logo email" width="30px" id="email-contato"/><div class="espaco-lateral"></div>E-mail do Mateus</a></div><p class="par-contato">Descrição do E-mail do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
+<div ><a href="https://www.youtube.com/channel/UCAXCKVHXg5weD2zqFJ056QQ" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`youtube`,true)" onmouseout="hoverDesativo(`youtube`,true)"><img src="./assets/img/youtube-desativado.svg" alt="logo youtube" width="30px" id="youtube-contato"/><div class="espaco-lateral"></div>Canal no YouTube do Mateus</a></div><p class="par-contato">Descrição do canal no Youtube do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
+<div ><a href="http://lattes.cnpq.br/7572519952454058" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`cnpq`,true)" onmouseout="hoverDesativo(`cnpq`,true)"><div class="box30-30"><img src="./assets/img/cnpq-desativado.svg" alt="logo cnpq" width="30px" id="cnpq-contato"/></div><div class="espaco-lateral"></div>Lattes do Mateus</a></div><p class="par-contato">Descrição do Lattes do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
+<div ><a href="./assets/media/css.pdf" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`cv`,true)" onmouseout="hoverDesativo(`cv`,true)"><div class="box30-30"><img src="./assets/img/cv-desativado.svg" alt="logo cv" width="30px" id="cv-contato"/></div><div class="espaco-lateral"></div>Curriculum Vitae do Mateus</a></div><p class="par-contato">Descrição do Curriculum Vitae do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
+
+const corpoModelosPT = '<div class="destaque"><div class="logo"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><div class="logo-dir"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><h1>Seção em construção</h1><p>Nesta seção de <b>Modelos</b>, pretendo colocar os modelos <b>LaTeX</b> que desenvolvi e utilizo no dia a dia, o qual foi minha motivação inicial para ter um site anos atrás. Apesar de hoje em dia eu utilizar o <b>GitHub</b>, acho que ainda posso usar o site para esse fim, mostrando uma <i>preview</i> do modelo ao usuário interessado, em vez de apenas um arquivo README, por exemplo.</p></div>';
+const corpoModelosEN = '<div class="destaque"><div class="logo"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><div class="logo-dir"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><h1>Section under construction</h1><p>In this section of <b>Templates</b>, I intend to post the <b>LaTeX</b>\'s templates that I developed and use daily, which was my initial motivation for having a site years ago. Although nowadays I\'m a <b>GitHub</b> user, still think that the site can be used for this purpose, showing a preview of the template to the interested user, instead of only a README file, for example.</p></div>';
+
+const corpoPublicacoesPT = '<div class="destaque"><div class="logo"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><div class="logo-dir"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><h1>Seção em construção</h1><p>Nesta seção de <b>Publicações</b>, pretendo </p></div>';
+const corpoPublicacoesEN = '<div class="destaque"><div class="logo"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><div class="logo-dir"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><h1>Section under construction</h1><p>In this section of <b>Publications</b>, I intend to </p></div>';
+
+const corpoJogosPT = '<div class="destaque"><div class="logo"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><div class="logo-dir"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><h1>Seção em construção</h1><p>Nesta seção de <b>Jogos</b>, pretendo colocar os jogos que desenvolvo como exercícios de programação para vocês se divertirem. A princípio, o site só irá rodar jogos desenvolvidos em <b>JavaScript</b>.</p><p>Tem alguma ideia interessante de jogo para eu implementar? Entre em contato comigo!</p></div>';
+const corpoJogosEN = '<div class="destaque"><div class="logo"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><div class="logo-dir"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><h1>Section under construction</h1><p>In this section of <b>Games</b>, I intend to post the games that I develop as programming training exercises for you to have fun. At first, the site will only run games developed in <b>JavaScript</b>.</p><p>Do you have a interesting idea of a game for me to implement? Get in touch with me!</p></div>';
+
+function irHome () {
+  const corpo = document.querySelector('.corpo');
+  corpo.innerHTML = '<div class="logo"> \
+                        <img src="./assets/img/logo-mateus.svg" alt="Logo do meu site pessoal" width="100%"/> \
+                      </div> \
+                      <h1>Bem-Vindo ao meu site pessoal!</h1><hr /> \
+                      <div class="inlinha" ><b>Versão 1.00</b><hr class="fill" />Atualizado em 15 de julho de 2020</div><p>Primeira versão do site! Nesta versão:</p><ul><li>teste</li></ul> \
+                      <p> \
+                        Nessa versão do meu site pessoal, meu objetivo foi fazer uma \
+                        <i>SPA</i> (sigla do inglês "Sigle Page Application", que significa uma \
+                        aplicação/aplicativo de página única, isto é, existe apenas uma página \
+                        web e não uma para cada seção do site) usando apenas <b>HTML</b>,\
+                        <b>CSS</b> e <b>JavaScript</b>. Com isso, quando aprender algum \
+                        <i>Framework</i>, quero ver quais as diferenças para se obter o mesmo \
+                        resultado. \
+                      </p>';
+  ativo('home');
+}
+
+function irSobre () {
+  const corpo = document.querySelector('.corpo');
+  corpo.innerHTML = '<h1>Sobre</h1>\
+<div class="linha">\
+    <div class="img-esquerda">\
+      <img\
+        src="./assets/img/pre-form.jpeg"\
+        alt="Não carregou"\
+        width="80%"\
+      />\
+    </div>\
+    <div >\
+      <h2>Formação</h2>\
+      <hr />\
+      <br />\
+      Texto Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\
+    </div>\
+  </div>\
+  <div class="linha">\
+  <div class="img-direita">\
+    <img\
+      src="./assets/img/pre-form.jpeg"\
+      alt="Não carregou"\
+      width="80%"\
+    />\
+  </div>\
+    <div >\
+      <h2>Formação</h2>\
+      <hr />\
+      <br />\
+      Texto Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \
+    </div>\
+  </div>';
+  ativo('sobre');
+}
+
+function irModelos () {
+  const corpo = document.querySelector('.corpo');
+  if (lingua === 'portugues') {
+    corpo.innerHTML = corpoModelosPT;
+  } else {
+    corpo.innerHTML = corpoModelosEN;
+  }
+  ativo('modelos');
+}
+
+function irPublicacoes () {
+  const corpo = document.querySelector('.corpo');
+  if (lingua === 'portugues') {
+    corpo.innerHTML = corpoPublicacoesPT;
+  } else {
+    corpo.innerHTML = corpoPublicacoesEN;
+  }
+  ativo('publicacoes');
+}
+
+function irContato () {
+  const corpo = document.querySelector('.corpo');
+  corpo.innerHTML = corpoContato;
+  ativo('contato');
+}
+
+function irJogos () {
+  const corpo = document.querySelector('.corpo');
+  if (lingua === 'portugues') {
+    corpo.innerHTML = corpoJogosPT;
+  } else {
+    corpo.innerHTML = corpoJogosEN;
+  }
+  ativo('jogos');
+}
+
+const listadesecoes = ['home', 'sobre', 'modelos', 'contato', 'jogos', 'publicacoes'];
+
+function ativo (novasecao) {
+  let i;
+  for (i = 0; i < listadesecoes.length; i++) {
+    document.querySelector(`#${listadesecoes[i]}`).classList.remove('ativo');
+  }
+  document.querySelector(`#${novasecao}`).classList.add('ativo');
+}
+
+function hoverAtivo (logo, contato) {
+  if (contato) {
+    const troca = document.querySelector(`#${logo}-contato`);
+    troca.src = `./assets/img/${logo}-ativado.svg`;
+  } else {
+    const troca = document.querySelector(`#${logo}`);
+    troca.src = `./assets/img/${logo}-ativado.svg`;
+  }
+}
+function hoverDesativo (logo, contato) {
+  if (contato) {
+    const troca = document.querySelector(`#${logo}-contato`);
+    troca.src = `./assets/img/${logo}-desativado.svg`;
+  } else {
+    const troca = document.querySelector(`#${logo}`);
+    troca.src = `./assets/img/${logo}-desativado.svg`;
+  }
+}
+
+const trocalingua = document.querySelector('#trocalingua');
+let lingua = '';
+
+const dictSecoesAcoes = {
+  home: irHome,
+  sobre: irSobre,
+  modelos: irModelos,
+  contato: irContato,
+  jogos: irJogos,
+  publicacoes: irPublicacoes
+};
+
+function trocaLingua () {
+  if (lingua === 'portugues') {
+    trocalingua.innerHTML = 'versão em português';
+    lingua = 'ingles';
+    document.querySelector('#home').querySelector('a').innerHTML = 'Home';
+    document.querySelector('#sobre').querySelector('a').innerHTML = 'About';
+    document.querySelector('#modelos').querySelector('a').innerHTML = 'Templates';
+    document.querySelector('#contato').querySelector('a').innerHTML = 'Contact';
+    document.querySelector('#jogos').querySelector('a').innerHTML = 'Games';
+    document.querySelector('#publicacoes').querySelector('a').innerHTML = 'Publications';
+    document.querySelector('.desenvolvido').innerHTML = 'Developed by Mateus Souza Oliveira';
+    if (document.querySelector('.ativo')) {
+      const secaoAtual = document.querySelector('.ativo').id;
+      dictSecoesAcoes[secaoAtual]();
+    }
+  } else {
+    trocalingua.innerHTML = 'english version';
+    lingua = 'portugues';
+    document.querySelector('#home').querySelector('a').innerHTML = 'Início';
+    document.querySelector('#sobre').querySelector('a').innerHTML = 'Sobre';
+    document.querySelector('#modelos').querySelector('a').innerHTML = 'Modelos';
+    document.querySelector('#contato').querySelector('a').innerHTML = 'Contato';
+    document.querySelector('#jogos').querySelector('a').innerHTML = 'Jogos';
+    document.querySelector('#publicacoes').querySelector('a').innerHTML = 'Publicações';
+    document.querySelector('.desenvolvido').innerHTML = 'Desenvolvido por Mateus Souza Oliveira';
+    if (document.querySelector('.ativo')) {
+      const secaoAtual = document.querySelector('.ativo').id;
+      dictSecoesAcoes[secaoAtual]();
+    }
+  }
+}
+
+function margemAutomatica () {
+  let alturaCabecalho = document.querySelector('.cabecalho').offsetHeight;
+  alturaCabecalho += 50;
+  document.querySelector('.corpo').style.marginTop = `${alturaCabecalho}px`;
+}
+
+// fazer função para mudar aspecto do rodape e cabecalho se largura menor que 750px
+
+trocaLingua();
+irHome();
+margemAutomatica();
+window.addEventListener('resize', margemAutomatica);
