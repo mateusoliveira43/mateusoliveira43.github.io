6281979702dc7addac0fb1b36a7819a09c511f3b
mateusoliveira43 <matews1943@gamil.com>
2020-07-15 20:51:18 -0300

    Atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 const corpoContato = '<h1>Contato</h1>\
 <div ><a href="https://www.linkedin.com/in/mateus-oliveira-2492851b1/" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`linkedin`,true)" onmouseout="hoverDesativo(`linkedin`,true)"><img src="./assets/img/linkedin-desativado.svg" alt="logo linkedin" width="30px" id="linkedin-contato"/><div class="espaco-lateral"></div>Linkedin do Mateus</a></div><p class="par-contato">Descrição do Linkedin do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
 <div ><a href="https://github.com/mateusoliveira43" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`github`,true)" onmouseout="hoverDesativo(`github`,true)"><img src="./assets/img/github-desativado.svg" alt="logo github" width="30px" id="github-contato"/><div class="espaco-lateral"></div>GitHub do Mateus</a></div><p class="par-contato">Descrição do GitHub do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
-<div ><a href="mailto:matews1943@gmail.com" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`email`,true)" onmouseout="hoverDesativo(`email`,true)"><img src="./assets/img/email-desativado.svg" alt="logo email" width="30px" id="email-contato"/><div class="espaco-lateral"></div>E-mail do Mateus</a></div><p class="par-contato">Descrição do E-mail do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
+<div ><a class="linha-contato" onmouseover="hoverAtivo(`email`,true)" onmouseout="hoverDesativo(`email`,true)"><img src="./assets/img/email-desativado.svg" alt="logo email" width="30px" id="email-contato"/><div class="espaco-lateral"></div>Me envie um e-mail: matews1943 at gmail.com</a></div><p class="par-contato">Descrição do E-mail do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
 <div ><a href="https://www.youtube.com/channel/UCAXCKVHXg5weD2zqFJ056QQ" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`youtube`,true)" onmouseout="hoverDesativo(`youtube`,true)"><img src="./assets/img/youtube-desativado.svg" alt="logo youtube" width="30px" id="youtube-contato"/><div class="espaco-lateral"></div>Canal no YouTube do Mateus</a></div><p class="par-contato">Descrição do canal no Youtube do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
 <div ><a href="http://lattes.cnpq.br/7572519952454058" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`cnpq`,true)" onmouseout="hoverDesativo(`cnpq`,true)"><div class="box30-30"><img src="./assets/img/cnpq-desativado.svg" alt="logo cnpq" width="30px" id="cnpq-contato"/></div><div class="espaco-lateral"></div>Lattes do Mateus</a></div><p class="par-contato">Descrição do Lattes do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
 <div ><a href="./assets/media/css.pdf" target="_blank" class="linha-contato" onmouseover="hoverAtivo(`cv`,true)" onmouseout="hoverDesativo(`cv`,true)"><div class="box30-30"><img src="./assets/img/cv-desativado.svg" alt="logo cv" width="30px" id="cv-contato"/></div><div class="espaco-lateral"></div>Curriculum Vitae do Mateus</a></div><p class="par-contato">Descrição do Curriculum Vitae do mateus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
@@ -16,7 +16,7 @@ const corpoPublicacoesEN = '<div class="destaque"><div class="logo"><img src="./
 const corpoJogosPT = '<div class="destaque"><div class="logo"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><div class="logo-dir"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><h1>Seção em construção</h1><p>Nesta seção de <b>Jogos</b>, pretendo colocar os jogos que desenvolvo como exercícios de programação para vocês se divertirem. A princípio, o site só irá rodar jogos desenvolvidos em <b>JavaScript</b>.</p><p>Tem alguma ideia interessante de jogo para eu implementar? Entre em contato comigo!</p></div>';
 const corpoJogosEN = '<div class="destaque"><div class="logo"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><div class="logo-dir"><img src="./assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%"/></div><h1>Section under construction</h1><p>In this section of <b>Games</b>, I intend to post the games that I develop as programming training exercises for you to have fun. At first, the site will only run games developed in <b>JavaScript</b>.</p><p>Do you have a interesting idea of a game for me to implement? Get in touch with me!</p></div>';
 
-function irHome () {
+function irHome() {
   const corpo = document.querySelector('.corpo');
   corpo.innerHTML = '<div class="logo"> \
                         <img src="./assets/img/logo-mateus.svg" alt="Logo do meu site pessoal" width="100%"/> \
@@ -35,7 +35,7 @@ function irHome () {
   ativo('home');
 }
 
-function irSobre () {
+function irSobre() {
   const corpo = document.querySelector('.corpo');
   corpo.innerHTML = '<h1>Sobre</h1>\
 <div class="linha">\
@@ -71,7 +71,7 @@ function irSobre () {
   ativo('sobre');
 }
 
-function irModelos () {
+function irModelos() {
   const corpo = document.querySelector('.corpo');
   if (lingua === 'portugues') {
     corpo.innerHTML = corpoModelosPT;
@@ -81,7 +81,7 @@ function irModelos () {
   ativo('modelos');
 }
 
-function irPublicacoes () {
+function irPublicacoes() {
   const corpo = document.querySelector('.corpo');
   if (lingua === 'portugues') {
     corpo.innerHTML = corpoPublicacoesPT;
@@ -91,13 +91,13 @@ function irPublicacoes () {
   ativo('publicacoes');
 }
 
-function irContato () {
+function irContato() {
   const corpo = document.querySelector('.corpo');
   corpo.innerHTML = corpoContato;
   ativo('contato');
 }
 
-function irJogos () {
+function irJogos() {
   const corpo = document.querySelector('.corpo');
   if (lingua === 'portugues') {
     corpo.innerHTML = corpoJogosPT;
@@ -109,7 +109,7 @@ function irJogos () {
 
 const listadesecoes = ['home', 'sobre', 'modelos', 'contato', 'jogos', 'publicacoes'];
 
-function ativo (novasecao) {
+function ativo(novasecao) {
   let i;
   for (i = 0; i < listadesecoes.length; i++) {
     document.querySelector(`#${listadesecoes[i]}`).classList.remove('ativo');
@@ -117,7 +117,7 @@ function ativo (novasecao) {
   document.querySelector(`#${novasecao}`).classList.add('ativo');
 }
 
-function hoverAtivo (logo, contato) {
+function hoverAtivo(logo, contato) {
   if (contato) {
     const troca = document.querySelector(`#${logo}-contato`);
     troca.src = `./assets/img/${logo}-ativado.svg`;
@@ -126,7 +126,7 @@ function hoverAtivo (logo, contato) {
     troca.src = `./assets/img/${logo}-ativado.svg`;
   }
 }
-function hoverDesativo (logo, contato) {
+function hoverDesativo(logo, contato) {
   if (contato) {
     const troca = document.querySelector(`#${logo}-contato`);
     troca.src = `./assets/img/${logo}-desativado.svg`;
@@ -148,7 +148,7 @@ const dictSecoesAcoes = {
   publicacoes: irPublicacoes
 };
 
-function trocaLingua () {
+function trocaLingua() {
   if (lingua === 'portugues') {
     trocalingua.innerHTML = 'versão em português';
     lingua = 'ingles';
@@ -180,7 +180,7 @@ function trocaLingua () {
   }
 }
 
-function margemAutomatica () {
+function margemAutomatica() {
   let alturaCabecalho = document.querySelector('.cabecalho').offsetHeight;
   alturaCabecalho += 50;
   document.querySelector('.corpo').style.marginTop = `${alturaCabecalho}px`;
