58e840de10496c94826965408c15851b8067bb65
Mateus Oliveira <matews1943@gmail.com>
2020-10-20 08:24:14 -0300

    version 2.01: implementation of templates section

--- linha.py
+++ linha.py
@@ -1,21 +1,10 @@
-with open('linha.txt', 'w') as writer:
-    body = '''
-<div class="destaque">
-  <div class="logo m-2 float-left">
-    <img src="./public/assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%" />
-  </div>
-  <div class="logo m-2 float-right">
-    <img src="./public/assets/img/manutencao.svg" alt="Logo de manuntenção" width="100%" />
-  </div>
-  <h1>Seção em construção</h1>
-  <p>
-    Nesta seção de <b>Modelos</b>, pretendo disponibilizar os links para os repositórios do <b>GitHub</b>
-    dos modelos <b>LaTeX</b> que desenvolvi e utilizo no dia a dia. Disponibilizá-los online foi minha
-    motivação inicial para ter um site anos atrás.
-  </p>
-</div>
+body = '''
+
     '''
+
+with open('linha.txt', 'w') as writer:
     body = body.replace('\n', '')
+    body = body.replace('   ', ' ')
     body = body.replace('  ', ' ')
-    body = body.replace("'", '\'')
+    body = body.replace("'", "\\'")
     writer.write(body)
