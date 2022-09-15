24f0f95c628f3a9061b7e5a382a4a525b2651d23
Mateus Oliveira <matews1943@gmail.com>
2022-09-15 16:35:15 -0300

    feat: Update website

--- docker/up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ROOT="$(dirname $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P ))"
-$ROOT/scripts/env.sh
-
-docker-compose \
---file $ROOT/docker/docker-compose.yaml \
---project-directory $ROOT \
-up
